b84f0acd3feda68dcf83914e2ba60010c04e7382
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-05-25 20:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+   let table = document.getElementById("sampleTable");
+  var row = table.insertRow(0);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
